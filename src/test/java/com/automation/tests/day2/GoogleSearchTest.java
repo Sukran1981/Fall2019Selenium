c792077ff2b1a043bc606e09606b037e8834244f
First name Last name <58491627+Sukran1981@users.noreply.github.com>
2020-03-04 14:49:25 -0500

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day2/GoogleSearchTest.java
@@ -0,0 +1,36 @@
+package com.automation.tests.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearchTest {
+
+    public static void main(String[] args) throws Exception{
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://google.com");
+
+        Thread.sleep(2000);
+        WebElement search =driver.findElement(By.name("q"));
+
+        //once we found an element,how to enter text?
+
+        search.sendKeys("java",Keys.ENTER);
+        Thread.sleep(2000);
+
+        WebElement news=driver.findElement(By.linkText("News"));
+        news.click();
+
+        driver.quit();
+    }
+}
+//name----it's one of the selenium locators
+//there are 8 locators
+//we use locators to find elements
+//to choose locator,just use By.locators
