c0f7adbe65d8c9e8af32483441d2b03a6c803e1e
First name Last name <58491627+Sukran1981@users.noreply.github.com>
2020-03-04 18:51:32 -0500

    created basic navigation class

--- src/test/java/com/automation/utilities/DriverFactory.java
+++ src/test/java/com/automation/utilities/DriverFactory.java
@@ -6,8 +6,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
-
-    public static WebDriver createADriver(String browserName){
+    /**
+     * you homework will be improve this method
+     * This method return webdriver object based on browser type
+     * if you want to use chrome browser,just provide chrome  as sparameter
+     * @param browserName
+     * @return webdriver object
+     */
+    public static WebDriver createDriver(String browserName){
 
         if(browserName.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
