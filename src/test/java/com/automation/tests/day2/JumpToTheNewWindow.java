d4936c9364a3f375408ef09e68c9a66ae7fe1cac
First name Last name <58491627+Sukran1981@users.noreply.github.com>
2020-03-04 11:13:26 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
+++ src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Set;
+
 public class JumpToTheNewWindow {
 
     public static void main(String[] args) throws Exception{
@@ -23,10 +25,43 @@ public class JumpToTheNewWindow {
         //based on window handle,we can switch in between windows
 
 
-        String windowHandle=driver.getWindowHandle();
+        String windowHandle = driver.getWindowHandle();
 
 
         System.out.println(windowHandle);
 
+
+        Set<String> windowHandles=driver.getWindowHandles();
+
+        System.out.println(windowHandles);
+
+        System.out.println("BEFORE SWITCH:"+driver.getCurrentUrl());
+
+        //since we have all windows
+        //and we know id of original window
+        //we can say switch to something that is not equals to old window id
+
+        for(String windowId :windowHandles){
+
+            if(!windowId.equals(windowHandle)){
+                driver.switchTo().window(windowId);
+            }
+        }
+
+        System.out.println("AFTER SWITCH :"+driver.getCurrentUrl());
+driver.close();
+    }
+
+    public static void switchToWindowBasedOnTitle(String pageTitle ,WebDriver driver){
+
+        Set<String> windows=driver.getWindowHandles();
+
+        for(String window: windows){
+            driver.switchTo().window(window);
+            if(driver.getTitle().equals(pageTitle)){
+
+                break;
+            }
+        }
     }
 }
