b19309c8ecae1cd86d4155acd2feccac733a0b26
First name Last name <58491627+Sukran1981@users.noreply.github.com>
2020-03-27 23:53:02 -0400

    HomeWork_3

--- src/test/java/com/automation/tests/homework1/Test_7.java
+++ src/test/java/com/automation/tests/homework1/Test_7.java
@@ -4,6 +4,7 @@ import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -23,13 +24,14 @@ public class Test_7 {
         BrowserUtils.wait(3);
 
 
-        driver.findElement(By.id("file-upload")).sendKeys("");
+        driver.findElement(By.id("file-upload")).sendKeys("/Users/sukran2019/Desktop/sukran1upload.txt");
         BrowserUtils.wait(3);
 
-        driver.findElement(By.id("file-submit")).click();
+        WebElement file_Uploaded= driver.findElement(By.id("file-submit"));
+        file_Uploaded.click();
         BrowserUtils.wait(3);
 
-        String actual=driver.findElement(By.xpath("h3")).getText();
+        String actual=driver.findElement(By.tagName("h3")).getText();
         String expected="File Uploaded!";
 
         Assert.assertEquals(actual,expected);
